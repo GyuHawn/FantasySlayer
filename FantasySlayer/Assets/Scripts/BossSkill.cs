e2bf12b7550d69be42cde73448bae45ecc5a2ee8
GyuHawn <knm128882@gmail.com>
2023-10-21 00:44:57 +0900

    보스 스킬 구현

--- FantasySlayer/Assets/Scripts/BossSkill.cs
+++ FantasySlayer/Assets/Scripts/BossSkill.cs
@@ -12,10 +12,26 @@ public class BossSkill : MonoBehaviour
     public int skillNumber; // 사용횟수
 
     // 스킬 2
+    public GameObject[] tSkillSpwan;
+    public GameObject tSkillEnermy;
 
-    private void Start()
+    // 스킬 3
+    public GameObject sSkillSpwan;
+    public GameObject sSkillEnermy;
+
+    private void Start()    
     {
-        // UseSkill(Skill1); // 스킬사용예시
+        //UseSkill(Skill3);
+        StartCoroutine(UseSkillsInSequence());
+    }
+
+    private IEnumerator UseSkillsInSequence()
+    {
+        UseSkill(Skill1);
+        yield return new WaitForSeconds(10);
+        UseSkill(Skill2);
+        yield return new WaitForSeconds(5);
+        UseSkill(Skill3);
     }
 
     private void UseSkill(Func<IEnumerator> skill)
@@ -47,14 +63,36 @@ public class BossSkill : MonoBehaviour
 
     IEnumerator Skill2()
     {
+        tSkillEnermy.SetActive(true);
 
-        yield break;
+        yield return new WaitForSeconds(2);
+
+        tSkillEnermy.SetActive(false);
+
+        for (int i = 0; i < tSkillSpwan.Length; i++)
+        {
+            tSkillSpwan[i].SetActive(true);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        yield return new WaitForSeconds(2);
+
+        for (int i = 0; i < tSkillSpwan.Length; i++)
+        {
+            tSkillSpwan[i].SetActive(false);
+        }
     }
 
     IEnumerator Skill3()
     {
+        sSkillEnermy.SetActive(true);
+        yield return new WaitForSeconds(2);
+        sSkillEnermy.SetActive(false);
+        sSkillSpwan.SetActive(true);
 
-        yield break;
+        yield return new WaitForSeconds(5);
+        sSkillSpwan.SetActive(false);
+
     }
 
 
